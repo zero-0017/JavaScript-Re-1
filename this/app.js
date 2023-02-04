a7be7a3f4cc89e5cf6927d9a5d0bbc200e39d16f
zero-0017 <hiromichi.v.0017@gmail.com>
2023-02-04 14:34:19 +0000

    [javascript]7章

---
+++ this/app.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('.bg1').on('click', function(){
+    $('.bg1').slideUp();
+  });
+
+  $('.bg2').on('click', function(){
+    $('.bg2').slideUp();
+  });
+
+  $('.bg3').on('click', function(){
+    $('.bg3').slideUp();
+  });
+
+  $('.bg4').on('click', function(){
+    $('.bg4').slideUp();
+  });
+});
